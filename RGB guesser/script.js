42a95d5643a3332c7fb6520eaa034e89585be713
Kacper <kacperprochwicz@op.pl>
2022-11-03 12:07:53 +0100

    Add files via upload

--- RGB guesser/script.js
+++ RGB guesser/script.js
@@ -5,11 +5,13 @@ var buttons4 = document.querySelector(".btn4");
 var buttons5 = document.querySelector(".btn5");
 var buttons6 = document.querySelector(".btn6");
 var button_reset = document.querySelector(".restart_btn");
+var napis = document.querySelector(".rgb");
+var correct_color;
 
-window.onload = setRandomColors();
+window.onload = setRandomColors(), pickWinner();
 
 button_reset.addEventListener("click",function(){
-    setRandomColors();
+    window.location.reload();
 });
 
 function changeColor(element){
@@ -26,4 +28,26 @@ function setRandomColors(){
     changeColor(buttons4);
     changeColor(buttons5);
     changeColor(buttons6);
+}
+
+function changeWinnerColor(element){
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    element.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    napis.innerHTML = `RGB : (${red},${green},${blue})`;
+    correct_color = `rgb(${red},${green},${blue})`;
+}
+
+function pickWinner(){
+    let winning_num = Math.floor(Math.random()*(7 - 1) + 1);
+    let winning_btn = document.querySelector(`.btn${winning_num}`);
+    changeWinnerColor(winning_btn);
+
+    winning_btn.addEventListener("click",function() {
+        napis.innerHTML = "CORRECT";
+        document.body.style.backgroundColor = correct_color;
+    })
+
+
 }
